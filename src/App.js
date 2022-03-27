25c85d38811f2ac196ef5dc05a088d90bd9b01b1
globefeng <feng.wang@esc4.net>
2022-03-26 22:03:45 -0500

    update homepage

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Homepage
+          Homepage changed
         </p>
         <a
           className="App-link"
