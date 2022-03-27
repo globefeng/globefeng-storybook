416888ca77fcf12e74bc43f9050277267ea42d84
globefeng <feng.wang@esc4.net>
2022-03-26 22:11:34 -0500

    update app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Homepage changed
+          Homepage change to deploy
         </p>
         <a
           className="App-link"
