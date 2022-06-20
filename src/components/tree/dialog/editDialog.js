4bc1066b8d4f8d9e82122d5b04d936c39dea4bb5
globefeng <globefeng@yahoo.com>
2022-06-19 21:54:07 -0500

    update textfield

--- src/components/tree/dialog/editDialog.js
+++ src/components/tree/dialog/editDialog.js
@@ -1,33 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Dialog, DialogTitle, DialogContent, Button, DialogActions } from '@material-ui/core';
 import TextField from '@mui/material/TextField';
 
 const EditDialog = ({node, onClose}) => {
+    const [text, setText] = useState('');
+
+    useEffect(() => {
+        if (node) setText(node.path)
+    }, [node]);
+
     return (
-        <Dialog
-            maxWidth={'sm'}
-            fullWidth={true}
-            open={node !== null}
-        >
+        <Dialog maxWidth={'sm'} fullWidth={true} open={node !== null}>
             <DialogTitle>Edit</DialogTitle>
             <DialogContent dividers>
-            {node && 
-                <TextField
-                autoFocus
-                margin="dense"
-                id="name"
-                label="Name"
-                type="text"
-                fullWidth
-                variant="standard"
-                value={node.path}
-                />
-            }
+                {node && 
+                    <TextField
+                    autoFocus
+                    margin="dense"
+                    id="name"
+                    label="Name"
+                    type="text"
+                    fullWidth
+                    variant="outlined"
+                    value={text}
+                    onChange={(e) => setText(e.target.value)}
+                    />
+                }
             </DialogContent>
             <DialogActions>
-            <Button onClick={onClose}>Cancel</Button>
-            <Button onClick={onClose}>Ok</Button>
+                <Button onClick={onClose}>Cancel</Button>
+                <Button onClick={onClose}>Ok</Button>
             </DialogActions>
         </Dialog>
     )
